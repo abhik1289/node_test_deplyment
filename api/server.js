fc12769e87e8497d4e3f4b740973060a9436a6e8
Abhik Patra <theabhik2020@gmail.com>
2024-10-06 17:01:44 +0530

    first commit

---
+++ api/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const user = require("../models/userModel");
+const app = express();
+
+mongoose.connect("mongodb+srv://note_app:IhdFZ86Glm50KLO4@cluster0.dovrp.mongodb.net/Note").then(() => {
+    console.log("Connected to Mongo")
+}).catch((err) => {
+    console.log(err)
+});
+
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extends: false }));
+
+app.get("/", (req, res) => {
+    res.send("Welcome")
+    console.log("first")
+})
+
+app.post("/create", async (req, res) => {
+    const { name, email, password } = req.body;
+    const data = await user.create({ name, email, password });
+    res.json({ data })
+})
+
+// console.log("first")
+
+
+
+
+app.listen(5000, () => {
+    console.log("Server run successfully")
+});
